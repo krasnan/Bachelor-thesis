cebd3017917e5cf9c9c5530c7b18f3db57f1a4ec
krasnan <martin.krasnan@gmail.com>
2015-10-26 18:30:07 +0100

    final version of web page

---
+++ Sources/Web/js/config.js
@@ -0,0 +1,15 @@
+var tSources = {
+    'Názov zdroja':'#'
+};
+var pSources = {
+    'Názov zdroja':'#'
+};
+var diary = {
+    '24.10.2015':'Navrhnutá webová stránka bakalárskej práce.',
+    '25.10.2015':'Dokončená finálna stránka bakalárskej práce.',
+};
+var download = {
+    'prezentaciaZdrojov':'#',
+    'vychodiskovaKapitoloa':'#',
+    'finalnaPraca':'#'
+};
