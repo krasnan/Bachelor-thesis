0d3429045c04ceec00d53da9c34f9640588641ab
Martin Krasňan <martin.krasnan@gmail.com>
2015-11-26 00:12:25 +0100

    added resources

--- Sources/Web/js/config.js
+++ Sources/Web/js/config.js
@@ -1,15 +1,21 @@
 var tSources = {
-    'Zdroje budú pridané čoskoro':'#'
+    'Objem a povrch gule <br>(Bakalárska práca, autor: Matúš Ivan)':'https://is.cuni.cz/webapps/zzp/download/130082037/?lang=cs',
+    'Motion Graphs in combination with Blender <br>(Bakalárska práca, autor: Dominik Kapišinský)':'http://www.dcs.fmph.uniba.sk/bakalarky/obhajene/getfile.php/kapisinsky_dominik_bc.zip?id=201&fid=395&type=application%2Foctet-stream',
+    'Použitie LabanXML v kombinácii s Blenderom <br>(Bakalárska práca, autor: Ján Ruhalovský)':'http://www.dcs.fmph.uniba.sk/bakalarky/obhajene/getfile.php/RuhalovskyJan.pdf?id=212&fid=416&type=application%2Fpdf',
+    'Procedurálny model ruky so zameraním na deformáciu geometrie  a vytváranie vrások <br>(Bakalárska práca, autor: Ján Špaček)':'http://js.poriadne.sk/files/Bakalarka.pdf',
+    'Archimedov zákon <br> (sk.wikipedia.org)':'https://sk.wikipedia.org/wiki/Archimedov_z%C3%A1kon'
 };
 var pSources = {
-    'Zdroje budú pridané čoskoro':'#'
+    'Blender 2.65.5 - API dokumentácia':'http://www.blender.org/api/blender_python_api_2_65_5/contents.html#',
+    'Python 3.4 - dokumentácia':'https://docs.python.org/',
 };
 var diary = {
     '24.10.2015':'Navrhnutá webová stránka bakalárskej práce.',
     '25.10.2015':'Dokončená finálna stránka bakalárskej práce.',
+    '25.11.2015':'Zozbierané zdroje k bakalárskej práci.'
 };
 var download = {
-    'prezentaciaZdrojov':'#',
+    'prezentaciaZdrojov':'./files/prezentacia_zdrojov.pdf',
     'vychodiskovaKapitoloa':'#',
     'finalnaPraca':'#'
 };
